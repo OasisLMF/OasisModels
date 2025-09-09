f7c015977731ea544e72e5026c341e8fcaa81d69
Ben Hayes <benhayes@benhayes.lan>
2025-09-09 16:11:45 +0100

    full uk pf

--- UKWind/get_footprint.sh
+++ UKWind/get_footprint.sh
@@ -1,5 +1,7 @@
 FP_URL="https://oasislmf-model-library-piwind-fulluk.s3.eu-west-1.amazonaws.com/footprint.bin.z"
 IDX_URL="https://oasislmf-model-library-piwind-fulluk.s3.eu-west-1.amazonaws.com/footprint.idx.z"
+EXP_URL="https://oasislmf-model-library-piwind-fulluk.s3.eu-west-1.amazonaws.com/location_full.parquet"
 
 wget "$FP_URL" -O model_data/footprint.bin.z
 wget "$IDX_URL" -O model_data/footprint.idx.z
+wget "$EXP_URL" -O tests/test_1/location_full.parquet"
