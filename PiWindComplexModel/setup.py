742d7ae2fd35288001122340ed92946579990264
Sam Gamble <hexadessa@gmail.com>
2026-06-05 09:42:12 +0100

    f

--- PiWindComplexModel/setup.py
+++ PiWindComplexModel/setup.py
@@ -4,8 +4,6 @@ setup(
     packages=find_packages(),
     name='OasisLMF_ComplexModelExample',
     version='1.0.0.0',
-    packages=find_packages(where='.'),
-    package_dir={'': '.'},
     entry_points={
         'console_scripts': [
             'OasisLMF_ComplexModelExample_gulcalc=complex_model_wrapper.OasisLMF_ComplexModelExample_gulcalc:main'
